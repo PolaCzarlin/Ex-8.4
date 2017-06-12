2c987723ebd94ce66a19117450110dcebb696328
Aga Sipowicz <agns22@wp.pl>
2017-06-13 01:33:15 +0200

    JS file

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js 
+var user = prompt('Type your name'); // zainicjowanie zmiennej
+alert( user +' You have been trolled ;)');
+console.log(user +'You have been trolled ;)');
+/* 
+Ważne:
+	Odpowiada dokładnie za to samo, co alert,
+	 z tym że wartość zostanie
+	  wyświetlona w zakładce Konsola w narzędziach deweloperskich.
+*/
